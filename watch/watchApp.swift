e96d4138973e7023f93775e923a64975fa69b409
cristianceban <cebancristi4444@gmail.com>
2024-03-02 21:36:56 +0200

    feat(watch-expo-plugin): update watch app ui

--- watch/watchApp.swift
+++ watch/watchApp.swift
@@ -1,14 +1,27 @@
+//
+//  Connectivity.swift
+//  watch
+//
+//  Created by Cristian Ceban on 02.03.2024.
+//
+
 import SwiftUI
 
 
 struct ContentView: View {
+  
+  @AppStorage(ConnectivityUserInfoKey.text.rawValue)
+  private var text: String?
+  
     var body: some View {
-        Text("Hello from Expo!")
+        Text("Hello from Expo! your text is: \(text ?? "")")
     }
 }
 
 @main
 struct watchApp: App {
+  let connectivity = Connectivity.shared
+  
     var body: some Scene {
         WindowGroup {
             ContentView()
