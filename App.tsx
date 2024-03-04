e202fa3cd6f5ac8b9f474ce5ccc456ca83e90205
cristianceban <cebancristi4444@gmail.com>
2024-03-04 21:00:55 +0200

    feat(watch-expo-plugin): update app text

--- App.tsx
+++ App.tsx
@@ -33,7 +33,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Testing Watch Connectivity</Text>
       <TextInput
         style={styles.input}
         onChangeText={onChangeText}
